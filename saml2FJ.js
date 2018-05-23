b58854418910a3b62a14f21de439b7035d73d548
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2018-05-23 15:08:20 -0300

    Fixed to new saml changes

--- saml2FJ.js
+++ saml2FJ.js
@@ -1,39 +1,42 @@
-
 var xml2json = require('xml2json');
 var parser = {
     toFiltredJSON: function (SAMLResponse, callback) {
 
         if (SAMLResponse == null || SAMLResponse == {} || SAMLResponse == '') {
-            callback({ error: true, description: "Null parameters" });
+            callback({
+                error: true,
+                description: "Null parameters"
+            });
         } else {
             var response = new Buffer(SAMLResponse, 'base64');
             var json = JSON.parse(xml2json.toJson(response.toString()));
             var filteredJSON = {};
-            var samlAttributes = json['samlp:Response']['saml:Assertion']['saml:AttributeStatement']['saml:Attribute'];
+            var samlAttributes = result['samlp:Response']['saml:Assertion'][0]['saml:AttributeStatement'][0]['saml:Attribute'];
             samlAttributes.forEach(function (element) {
-                switch (element['Name'].toLowerCase()) {
+                switch (element['$']['Name'].toLowerCase()) {
                     case "firstname":
-                        filteredJSON["firstName"] = element['saml:AttributeValue']['$t'];
+                        filteredJSON["firstName"] = element['saml:AttributeValue'][0]['_'];
                         break;
                     case "lastname":
-                        filteredJSON["lastName"] = element['saml:AttributeValue']['$t'];
+                        filteredJSON["lastName"] = element['saml:AttributeValue'][0]['_'];
                         break;
                     case "uid":
-                        filteredJSON["uid"] = element['saml:AttributeValue']['$t'];
+                        filteredJSON["uid"] = element['saml:AttributeValue'][0]['_'];
                         break;
                     case "emailaddress":
-                        filteredJSON["email"] = element['saml:AttributeValue']['$t'];
+                        filteredJSON["email"] = element['saml:AttributeValue'][0]['_'];
                         break;
                     case "cn":
-                        filteredJSON["fullName"] = element['saml:AttributeValue']['$t'];
+                        filteredJSON["fullName"] = element['saml:AttributeValue'][0]['_'];
                         break;
                     case "bluegroups":
-                        filteredJSON["blueGroups"] = JSON.parse(element['saml:AttributeValue']["$t"]).map(function (group) {
-                            return { name: group.split(',')[0].split('=')[1] }
+                        filteredJSON["blueGroups"] = JSON.parse(element['saml:AttributeValue'][0]["_"]).map(function (group) {
+                            return {
+                                name: group.split(',')[0].split('=')[1]
+                            }
                         });
                         break;
                 }
-
             });
 
             callback(filteredJSON);
@@ -41,11 +44,14 @@ var parser = {
     },
     toJSON: function (SAMLResponse, callback) {
         if (SAMLResponse == null || SAMLResponse == {} || SAMLResponse == '') {
-            callback({ error: true, description: "Null parameters" });
+            callback({
+                error: true,
+                description: "Null parameters"
+            });
         } else {
             var response = new Buffer(SAMLResponse, 'base64');
             var json = JSON.parse(xml2json.toJson(response.toString()));
-            
+
             callback(json);
         }
     }
